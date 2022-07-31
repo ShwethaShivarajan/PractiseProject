0391888b34dabb51fa3ade1c015a5128c79ff8d0
Shwetha Shivarajan <shwethashivarajan@gmail.com>
2022-07-31 12:20:49 +0530

    minor update

--- src/Collections/WordOccurance.java
+++ src/Collections/WordOccurance.java
@@ -8,27 +8,24 @@ public class WordOccurance {
 		// TODO Auto-generated method stub
 
 		String str1 = "She sells sea shells on the sea shore as She is sea shells seller";
-		
-		String [] words = str1.split(" ");
-		
+
+		String[] words = str1.split(" ");
+
 		HashMap<String, Integer> map1 = new HashMap<String, Integer>();
-		
-		
-		for(String word: words) {
-			
-			if(map1.get(word)==null) {
+
+		for (String word : words) {
+
+			if (map1.get(word) == null) {
 				map1.put(word, 1);
-				
-			}
-			else
-			{
+
+			} else {
 				int count = map1.get(word);
-				map1.put(word, count+1);
+				map1.put(word, count + 1);
 			}
-			
+
 		}
 		System.out.println(map1);
-		
+
 	}
 
 }
